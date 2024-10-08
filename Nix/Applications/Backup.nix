9363b03a680fe466bc2613ff3c63ffb3d87d9091
unsigned57 <unsigned57@gmail.com>
2024-10-08 09:35:34 +0800

    add syncthing

--- Nix/Applications/Backup.nix
+++ Nix/Applications/Backup.nix
@@ -4,6 +4,38 @@
   services = {
     syncthing = {
       enable = true;
+      user = "ephemeral";
+      dataDir = "/home/ephemeral/Documents";
+      configDir = "/home/ephemeral/Documents/.config/syncthing";
+      overrideDevices = true;
+      overrideFolders = true;
+      settings = {
+        devices = {
+          "pixel" = {
+            id = "45FRUN5-UQJ6ZMO-6ZD5AVC-5XCFSRT-NC36XLU-5WNFRE7-DWLEYMR-7ZUR4AR";
+          };
+        };
+        folders = {
+          "Music" = {
+            path = "/home/ephemeral/Music";
+            devices = [ "pixel" ];
+            ignorePerms = false;
+          };
+        };
+      };
+    };
+  };
+
+  networking = {
+    firewall = {
+      allowedTCPPorts = [
+        8384
+        22000
+      ];
+      allowedUDPPorts = [
+        22000
+        21027
+      ];
     };
   };
 
